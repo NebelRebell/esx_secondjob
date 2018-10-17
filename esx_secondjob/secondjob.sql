759bc709eb1b8d1c8d79a8b1bbdd9b19b93f14cd
NebelRebell <40825041+NebelRebell@users.noreply.github.com>
2018-10-17 11:03:26 +0200

    Update secondjob.sql

--- esx_secondjob/secondjob.sql
+++ esx_secondjob/secondjob.sql
@@ -5,31 +5,12 @@ ALTER TABLE `users`
 	ADD COLUMN `job_grade2` INT NULL DEFAULT 0 AFTER `job2`
 ;
 
-CREATE TABLE `jobs2` (
-	`name` varchar(50) NOT NULL,
-	`label` varchar(255) DEFAULT NULL,
 
-	PRIMARY KEY (`name`)
-);
+INSERT INTO `jobs` (`name`, `label`, `whitelisted`) VALUES
+('unemployed2',	'Arbeitsuchend',	0);
 
-INSERT INTO `jobs2` VALUES ('unemployed2','Arbeitslos');
-
-
-CREATE TABLE `job_grades2` (
-	`id` int(11) NOT NULL AUTO_INCREMENT,
-	`job_name` varchar(255) DEFAULT NULL,
-	`grade` int(11) NOT NULL,
-	`name` varchar(255) NOT NULL,
-	`label` varchar(255) NOT NULL,
-	`salary` int(11) NOT NULL,
-	`skin_male` longtext NOT NULL,
-	`skin_female` longtext NOT NULL,
-
-	PRIMARY KEY (`id`)
-);
-
-INSERT INTO `job_grades2` VALUES (1,'unemployed2',0,'rsa','Los Angeles',5,'{}','{}')
-;
+INSERT INTO `job_grades` (`job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
+('unemployed2',	0,	'Arbeitssuchender',	'Arbeitssuchender',	0,	'{}',	'{}');
 
 
 
